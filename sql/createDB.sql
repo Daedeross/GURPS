d0708d3d1a80223a464cbc1ae087daa47ff4c7a3
Bryan Jones <jone0751@umn.edu>
2014-09-18 16:28:11 -0500

    Working in Lab

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -1,4 +1,21 @@
-CREATE TABLE Characters {
+CREATE FUNCTION getThreshold(@maxVal int, @curVal int)
+RETURNS int
+AS
+BEGIN
+    DECLARE @ret int;
+	DECLARE @thresh float;
+	SET @thresh = (@maxVal - @curVal) / CONVERT (float, @maxVal);
+	SET @ret = CASE
+		WHEN @thresh > 0.3333 THEN 2
+		WHEN @thresh > 0 THEN 1
+		ELSE CEILING(@thresh)
+	END;
+	RETURN @ret;
+END
+GO
+
+CREATE TABLE Characters
+(
     CharacterId int IDENTITY(1,1) PRIMARY KEY,
     UserId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Users(Id),
     Name nvarchar(255),
@@ -11,36 +28,23 @@ CREATE TABLE Characters {
 	DX int,
 	IQ int,
 	HT int,
-	HP int
+	HP int,
 	FP int,
 	Per int,
 	Will int,
 	-- Encounter Stuff
 	BasicSpeed float(24),
 	InitiativeBonus int
-};
+)
 
-CREATE FUNCTION getThreshold(@maxVal int, @curVal int)
-RETURNS int
-AS
-BEGIN
-    DECLARE @ret int;
-	DECLARE @thresh float;
-	SET @thresh = (@maxVal - @curVal) / CONVERT (float, @maxVal);
-	SET @ret = CASE
-		WHERE @thresh > 0.3333 THEN 2
-		WHERE @thresh > 0 THEN 1
-		ELSE CEILING(@thresh)
-	END;
-	RETURN @ret;
-END;
-
-CREATE TABLE GmCharacters {
+CREATE TABLE GmCharacters
+(
 	GmId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Users(Id),
 	CharacterId int FOREIGN KEY REFERENCES Characters(CharacterId),
-}
+)
 
-CREATE TABLE EncounterCharacters {
+CREATE TABLE EncounterCharacters
+(
     Id int IDENTITY(1,1) PRIMARY KEY,
     EncounterId int NOT NULL FOREIGN KEY REFERENCES Encounters(EncounterId),
     CharacterId int NOT NULL FOREIGN KEY REFERENCES Characters(CharacterId),
@@ -55,29 +59,45 @@ CREATE TABLE EncounterCharacters {
 	InitiativeBonus int, 
     IsVisible bit DEFAULT TRUE,
     IsKO bit DEFAULT FALSE,
-};
+);
 
-CREATE TABLE Encounters {
+CREATE TABLE Encounters 
+(
 	EncounterId int IDENTITY(1,1) PRIMARy KEY,
 	Gm UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Users(Id),
-}
+);
 
-CREATE TABLE EcounterPlayers {
+CREATE TABLE EcounterPlayers
+(
     EncounterId int NOT NULL FOREIGN KEY REFERENCES Encounters(EncounterId),
 	PlayerId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Users(Id),
-}
+);
 
-CREATE TABLE EncounterGms {
+CREATE TABLE EncounterGms
+(
     EncounterId int NOT NULL FOREIGN KEY REFERENCES Encounters(EncounterId),
 	Gm UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Users(Id),
-}
+);
 
-CREATE TABLE EncounterRounds {
-	
-}
+CREATE TABLE EncounterRounds
+(
+	RoundId int IDENTITY(1,1) PRIMARY KEY,
+	EncounterId int NOT NULL FOREIGN KEY REFERENCES Encounters(EncounterId),
+	RoundNumer int
+);
 
-CREATE VIEW EncounterGMCharacters
-{
-    SELECT * FROM Characters c
-        INNER JOIN EncounterCharacters ec ON c.Id = ec.CharacterID
-};
+CREATE TABLE InitiativeTokens
+(
+	TokenId int IDENTITY(1,1) PRIMARY KEY,
+	RoundId int NOT NULL FOREIGN KEY REFERENCES EncounterRounds(RoundId),
+	CharacterId int NOT NULL,
+	Initiative int NOT NULL,
+	TieBreaker float NOT NULL,
+	FinalInitiative AS Initiative + TieBreaker
+);
+
+--CREATE VIEW EncounterGMCharacters
+--(
+--    SELECT * FROM Characters c
+--        INNER JOIN EncounterCharacters ec ON c.Id = ec.CharacterID
+--);
