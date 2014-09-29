7b0f69a46c130aba907e4d1dcad84b27d41515c5
jone0751 <jone0751@umn.edu>
2014-09-29 14:04:25 -0500

    fixed conflicts

--- GurpsWebTools/sql/getPlayerEncChars.sql
+++ GurpsWebTools/sql/getPlayerEncChars.sql
@@ -1,3 +1,7 @@
+CREATE PROCEDURE getPlayerEncCharacters
+	@encId int,
+	@playerId int
+AS
 SELECT * FROM Characters c
   INNER JOIN EncounterCharacters ec ON c.Id = ec.CharacterID
   WHERE ec.EncounterId = $encounterId
